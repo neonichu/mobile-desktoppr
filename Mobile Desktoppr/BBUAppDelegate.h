34f3529c991cc04555f1a66e24b6616ffc7b5a4c
Boris Bügling <b.buegling@gingco.net>
2013-01-14 11:54:57 +0100

    Initial Commit

---
+++ Mobile Desktoppr/BBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBUAppDelegate.h
+//  Mobile Desktoppr
+//
+//  Created by Boris Bügling on 14.01.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
