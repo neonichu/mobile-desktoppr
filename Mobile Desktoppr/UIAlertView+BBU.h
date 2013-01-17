632309028dd06ffa0d7779dca72d6abd9b8d6174
Boris Bügling <boris@icculus.org>
2013-01-17 21:17:23 +0100

    UIAlertView error helpers.

---
+++ Mobile Desktoppr/UIAlertView+BBU.h
@@ -0,0 +1,15 @@
+//
+//  UIAlertView+BBU.h
+//  Slope
+//
+//  Created by Boris Bügling on 05.01.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView (BBU)
+
++(instancetype)bbu_showAlertWithError:(NSError*)error;
+
+@end
---
+++ Mobile Desktoppr/UIAlertView+BBU.m
@@ -0,0 +1,23 @@
+//
+//  UIAlertView+BBU.m
+//  Slope
+//
+//  Created by Boris Bügling on 05.01.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import "UIAlertView+BBU.h"
+
+@implementation UIAlertView (BBU)
+
++(instancetype)bbu_showAlertWithError:(NSError*)error {
+    UIAlertView* alertView = [[[self class] alloc] initWithTitle:error.localizedDescription
+                                                         message:error.localizedRecoverySuggestion
+                                                        delegate:nil
+                                               cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                                               otherButtonTitles:nil];
+    [alertView show];
+    return alertView;
+}
+
+@end
