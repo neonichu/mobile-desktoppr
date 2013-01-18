60ab1a3dc323b900271e5c4612a42b71f40083dc
Boris Bügling <b.buegling@gingco.net>
2013-01-18 12:57:34 +0100

    Remove response-element from the JSON to make generating more useful.

--- Generated/gen.sh
+++ Generated/gen.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+removeResponse () {
+  sed -i '' -e 's/^{"response"://' -e 's/}$//' "$1"
+}
+
+rm -f *.h *.json *.m
+
 curl -s -o user.json https://api.desktoppr.co/1/users/keithpitt &&
+removeResponse user.json
 ../vendor/grenobj/grenobj.py user.json DesktopprUser
 
-curl -s -o wallpapers.json https://api.desktoppr.co/1/users/keithpitt/wallpapers && 
-../vendor/grenobj/grenobj.py wallpapers.json DesktopprPicture
+curl -s -o wallpaper.json https://api.desktoppr.co/1/users/keithpitt/wallpapers/random && 
+removeResponse wallpaper.json
+../vendor/grenobj/grenobj.py wallpaper.json DesktopprPicture
