5529c9167f1f353949ec87554ef2980e68a02010
Boris Bügling <boris@icculus.org>
2013-01-28 14:50:34 +0100

    Added compare phase to make this more useful.

--- Generated/gen.sh
+++ Generated/gen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+compare() {
+  echo "# $1"
+  sed -i '' -e "s/BBU$1/$1/g" "BBU$1.h" &&
+  diff -I '^// \|^$' "../Mobile Desktoppr/$1.h" "BBU$1.h"
+}
+
 removeResponse () {
   sed -i '' -e 's/^{"response"://' -e 's/}$//' "$1"
 }
@@ -7,9 +13,11 @@ removeResponse () {
 rm -f *.h *.json *.m
 
 curl -s -o user.json https://api.desktoppr.co/1/users/keithpitt &&
-removeResponse user.json
-../vendor/grenobj/grenobj.py user.json DesktopprUser
+removeResponse user.json &&
+../vendor/grenobj/grenobj.py user.json DesktopprUser &&
+compare DesktopprUser
 
 curl -s -o wallpaper.json https://api.desktoppr.co/1/users/keithpitt/wallpapers/random && 
-removeResponse wallpaper.json
-../vendor/grenobj/grenobj.py wallpaper.json DesktopprPicture
+removeResponse wallpaper.json &&
+../vendor/grenobj/grenobj.py wallpaper.json DesktopprPicture &&
+compare DesktopprPicture
